51f79ca077e020515a485cbad32c515733d62c56
Luis Ho <Luis.HoKu@jetblue.com>
2016-04-05 15:39:16 -0400

    first commit

---
+++ js/tictactoe.js
@@ -0,0 +1,121 @@
+angular.module("tictacApp",[])
+.controller("tictacCtrl",['$scope','$timeout',function($scope,$timeout){
+	$scope.resetGameAll=function(){
+		$scope.cPlayers=[
+			{
+				name:"Player 1",
+				spot:"x",
+				score:0
+			},{
+				name:"Computer",
+				spot:"o",
+				score:0
+			}
+		];
+		$scope.resetGame();
+	}
+	$scope.resetGame=function(){
+		$scope.tictac = {
+			"0":[null,null,null],
+			"1":[null,null,null],
+			"2":[null,null,null]
+		}
+		$scope.cMark="x";
+		$scope.Winner=null;
+		$scope.isOver=false;
+		$scope.cPlayer=$scope.cPlayers[0];
+	}
+	$scope.resetGameAll();
+	$scope.foundEmpty=function(){
+		var found=false;
+		angular.forEach($scope.tictac,function(row){
+			angular.forEach(row,function(col){
+				if(!col)
+				{
+					found=true;
+				}
+			})
+		})
+		if(!found){
+			$scope.isOver=true;
+		}
+		return found;
+	}
+	$scope.checkGame=function(){
+		var tictac=$scope.tictac;
+		var mark=$scope.cMark;
+		var found=false;
+		var winner=null;
+		if((tictac[0][0]==mark && tictac[0][1]==mark && tictac[0][2]==mark) || (tictac[1][0]==mark && tictac[1][1]==mark && tictac[1][2]==mark) || (tictac[2][0]==mark && tictac[2][1]==mark && tictac[2][2]==mark)){
+			found=true;
+		}
+		else if((tictac[0][0]==mark && tictac[1][0]==mark && tictac[2][0]==mark)|| (tictac[0][1]==mark && tictac[1][1]==mark && tictac[2][1]==mark) || (tictac[0][3]==mark && tictac[1][3]==mark && tictac[2][3]==mark)){
+			found=true;
+		}
+		else if((tictac[0][0]==mark && tictac[1][1]==mark && tictac[2][2]==mark) || (tictac[0][2]==mark && tictac[1][1]==mark && tictac[2][0]==mark)){
+			found=true;
+		}
+		if(found){
+			$scope.isOver=true;
+			$scope.Winner = $scope.cPlayers.filter(function(player){
+				return player.spot==$scope.cMark;
+			})[0]
+			var total=$scope.Winner.score;
+			total++;
+			$scope.Winner.score=total;
+			return true;
+		}
+		return false;
+	}
+	$scope.switchPlayer=function(){
+		$scope.cPlayer=$scope.cPlayers.filter(function(player){
+			return player.spot != $scope.cMark;
+		})[0]
+		$scope.cMark=($scope.cMark=="x")?"o":"x";
+	}
+	$scope.setSpot=function(row,col){
+		var num=Math.floor(Math.random()*8)
+		var found =false;
+		var stillOn=$scope.foundEmpty();
+		$scope.checkGame();
+		if(!$scope.isOver){
+			if(!$scope.tictac[row][col] && stillOn)
+			{
+				$scope.tictac[row][col]=$scope.cMark;
+				$scope.checkGame();
+				$scope.switchPlayer();
+				stillOn=$scope.foundEmpty();
+				if(!$scope.isOver){
+					found=false;
+					$timeout(function(){
+						while(!found){
+							var prow=Math.floor(Math.random()*3);
+							var pcol=Math.floor(Math.random()*3);
+							if(!$scope.tictac[prow][pcol] && stillOn)
+							{
+								$scope.tictac[prow][pcol]=$scope.cMark;
+								$scope.checkGame();
+								$scope.switchPlayer();
+								found=true;
+							}
+						}
+					},1000)
+				}
+				else
+				{
+					console.log("GAME OVER")
+				}
+			}
+			else{
+				console.log("it's taken")
+			}
+		}
+		else
+		{
+			console.log("GAME OVER")
+		}
+		if($scope.Winner  && $scope.isOver){
+			console.log("FOUND WINNER"+$scope.Winner.name)
+		}
+	}
+}]);
